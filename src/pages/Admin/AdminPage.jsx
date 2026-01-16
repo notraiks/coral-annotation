ea40c99947ef8125484eba1109415c787e379dce
notraiks <23101967@usc.edu.ph>
2026-01-16 23:28:43 +0800

    Admin Dashboard

--- src/pages/Admin/AdminPage.jsx
+++ src/pages/Admin/AdminPage.jsx
@@ -4,7 +4,7 @@ export default function AdminPage() {
     return (
         <div>
             <h1>Admin Page</h1>
-            <p>Welcome to the admin page.</p>
+            <p>This is the admin page</p>
         </div>
     );
 }
